8fe47218f02fc458755683aa3dd5575bb48a7cc6
Feliipe-Silva <phellipedantask800@gmail.com>
2023-12-12 20:46:30 -0300

    img e Css

--- index.php
+++ index.php
@@ -4,30 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Oficina Mecânica</title>
+    <link rel="stylesheet" href="principal.css">
+    
 </head>
 <body>
-    <h1>Oficina o Chicão</h1>
+    <h1>Oficina o Chicão <img src="img/imgOficina.png" alt=""></h1>
 <form action="processar.php" method="post">
 <hr>
-<table border="1">
+<table>
     <tr>
         <th>Item</th>
         <th>Quantidade</th>
     </tr>
     <tr>
         <td>Pneu</td>
-        <td><input type="number" name="pneu" id="pneu" placeholder="ex:123"></td>
+        <td><input type="number" name="pneu" id="pneu" min="0" value="0" placeholder="ex:123"></td>
     </tr>
     <tr>
         <td>Óleo</td>
-        <td><input type="number" name="oleo" id="oleo" placeholder="ex:123"></td>
+        <td><input type="number" name="oleo" id="oleo" min="0" value="0" placeholder="ex:123"></td>
     </tr>
     <tr>
         <td>Vela</td>
-        <td><input type="number" name="vela" id="vela" placeholder="ex:123"></td>
+        <td><input type="number" name="vela" id="vela" min="0" value="0" placeholder="ex:123"></td>
     </tr> 
- 
-</table> <br>
+</table>
+    <hr>
+
+    <tr>
+        <dt>Como você nos achou?</dt>
+        <dt>
+            <select name="achar" id="achar" size="1">
+                <option value="a">Sou cliente</option>
+                <option value="b">Anúncio de TV</option>
+                <option value="c">Telefone</option>
+                <option value="d">Boca a boca</option>  
+            </select>
+        </dt>
+    </tr>
+ <br>
 <input type="submit" value="Enviar">
 
 </form>
--- processar.php
+++ processar.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Oficina o Chicão</title>
+    <link rel="stylesheet" href="principal.css">
 </head>
 
 <body>
@@ -13,12 +14,17 @@
     <hr>
 
     <?php
-    setlocale(LC_TIME, 'pt_BR', 'pt_BR.UTF-8', 'pt_BR.utf-8', 'portuguese');
+    date_default_timezone_set("America/Sao_Paulo");
+    echo 'Pedido processado ás:' . date ("d /M /Y ") 
+    . '<br>';
+
     $pneu = $_POST['pneu'];  // criando variaveis 
     $oleo = $_POST['oleo'];  // criando variaveis . 
     $vela = $_POST['vela'];
+    $achar = $_POST['achar'];
     $valortotal = 0.00;
     $qtdtotal = 0;
+
 
     define ('PRECOPNEU', 100);
     define ('PRECOOLEO', 10);   
@@ -26,31 +32,59 @@
 
     $valortotal = ($pneu * PRECOPNEU) + ($oleo * PRECOOLEO) + ($vela * PRECOVELA);
 
-    echo '<p> Pedido processado ás: '
-        . strftime('%A, %d de %B de %Y', strtotime('today'))
-        . '</p>';
-
     echo $pneu . ' pneus <br>';
     echo $oleo . ' galões <br>';
     echo $vela . ' velas <br>';
     
-    //$valortotal = 0;
-    $qtdtotal = $pneu + $oleo + $vela;
 
+    $qtdtotal = $pneu + $oleo + $vela;
+    
     if ($qtdtotal == 0) {
         echo '<br> Adicione um produto!';   
-    } else {
-        echo '<br> A quantidade é: ' .$qtdtotal;
     };
-
+    echo '<br> A quantidade é: ' .$qtdtotal;
     //------------------------------------------------------------------------------
     echo '<hr>';
     
     if ($valortotal == 0) {
         echo 'SEM VALOR!';
     } else {
-        echo '<br> *O valor total a pagar é R$:' .number_format($valortotal, 2, ",","."); // FORMATAÇÃO DE NUMERO , .
+        echo '<br> *O subtotal (valor sem a taxa) é de R$:' .number_format($valortotal, 2, ",","."); // FORMATAÇÃO DE NUMERO , .
     };
+    $taxa = 0.22; // lucro presumindo
+
+    $valortotal = $valortotal * (1 + $taxa);
+    if ($pneu < 10) {
+        $desconto = 0;
+    } elseif (($pneu > 10) && ($pneu <= 49)) {
+        $desconto = 0.05;   
+    } elseif (($pneu > 49) & ($pneu <= 99)) {
+        $desconto = 0.10;
+    } else {
+        $desconto = 0.15;
+    }
+    
+    echo "<br> O valor total é de R$:" .number_format($valortotal, 2,",",".");
+    $valortotal = $valortotal * (1 - $desconto);
+    echo "<br> O valor total com desconto é de R$: " .number_format($valortotal, 2,",",".");
+
+    switch ($achar) {
+        case "a":
+            echo '<p>Cliente regular</p>';
+            break;
+        case 'b':
+            echo '<p>Anúncio de TV</p>';
+            break;
+        case 'c':
+            echo '<p>Telefone</p>';
+            break;
+        case 'd':
+            echo '<p>Boca a boca</p>';
+            break;
+        default:
+        echo 'Clente não encontrado!';
+    }
     ?>
 </body>
+
 </html>
